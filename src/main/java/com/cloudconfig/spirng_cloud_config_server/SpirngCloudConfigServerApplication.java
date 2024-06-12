4d61716c8c4f03ef24fd90fede3170a14071b593
vikasgupta1204 <vikasgupta1204.vg@gmail.com>
2024-06-12 20:02:42 +0530

    added config server

---
+++ src/main/java/com/cloudconfig/spirng_cloud_config_server/SpirngCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.cloudconfig.spirng_cloud_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngCloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudconfig/spirng_cloud_config_server/SpirngCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudconfig.spirng_cloud_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
